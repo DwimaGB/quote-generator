62eaff5373ad534988a951a065ac1c6eb0c3accb
Dwima Basumatary <dwima1583@gmail.com>
2023-04-10 19:08:10 +0530

    basic functionality done

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,37 @@
 
+const newQuoteBtn = document.getElementById('new-quote');
+const displayQuote = document.getElementById('quote');
+const displayAuthor = document.getElementById('author');
+const requestQuote = `${window.origin}/api`;
+
+
+window.addEventListener('load', async()=>{
+    const quote = await getQuote();
+    updateDisplay(quote);
+})
+
+newQuoteBtn.addEventListener('click', async()=>{
+    const quote = await getQuote();
+    updateDisplay(quote);
+    
+})
+
+
+function updateDisplay(quote){
+    displayQuote.innerText = quote.quote;
+    displayAuthor.innerText = quote.author;
+}
+
 
 async function getQuote(){
     try{
-        const response = await fetch('http://127.0.0.1:3000/api');
-        const quote = await response.json();
-        const parsedQuote = JSON.parse(quote)
-        console.log(parsedQuote);
+        const response = await fetch(requestQuote);
+        const data = await response.json();
+        const parsedData = JSON.parse(data);
+        return parsedData[0];
     }
     catch(e){
         alert(e);
     }
 }
+
